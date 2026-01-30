6b4dd4947302669b6b16dfb2b1abde5a2d3b0659
Jotish <jotishk649@gmail.com>
2026-01-30 12:49:07 +0530

    changes

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${import.meta.env.REACT_APP_API_URL}/api`,
   withCredentials: true,
 });
-console.log(import.meta.env.VITE_API_URL);
 
 // attach token if exists
 api.interceptors.request.use((config) => {
